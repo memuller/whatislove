7c1401e7466caad5f794db9426026067a4446210
Matheus E. Muller <hello@memuller.com>
2016-09-10 18:02:05 -0300

    Includes the Player class in the main LOVE program.

--- main.lua
+++ main.lua
@@ -1,18 +1,12 @@
 
+local Player = require('./lib/Player')
+
 function love.load()
-  x, y = 10,10
+  player = Player:new(200,200)
 end
 
 function love.update(dt)
-  if love.keyboard.isDown('right') then
-    x = x+ 10--*dt
-  end
-  if x > love.graphics.getWidth() - 100 then
-    x = 0
-    y = y +30
-  end
 end
 
 function love.draw()
-    love.graphics.print("Hello World", x, y)
 end
