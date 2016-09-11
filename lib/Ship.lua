b499ce8d3242d582c86c26732f5acddd9ff65cd8
Matheus E. Muller <hello@memuller.com>
2016-09-11 19:49:44 -0300

    Places Ships outside the screen, on their lanes.

--- lib/Ship.lua
+++ lib/Ship.lua
@@ -2,10 +2,18 @@ local Ship = {
   width = 20,
   height = 40
 }
+
 function Ship:new()
   local object = {}
   setmetatable(object, self)
   self.__index = self
+  object.x, object.y = Ship.place()
+  object.state = 'movingIn'
   return object
+end
+function Ship.place()
+  local lanes = math.floor((love.graphics.getWidth() -Ship.width*3)/Ship.width)
+  local place = math.random(lanes)
+  return Ship.width + (place*Ship.width), -Ship.height
 end
 return Ship
