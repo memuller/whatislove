d392d87c8f63f30d9b8bb6acee65e7ac70fb4443
Matheus E. Muller <hello@memuller.com>
2016-09-11 22:35:53 -0300

    Adds global Game object w/ a timer.

--- main.lua
+++ main.lua
@@ -4,16 +4,19 @@ local Ship = require('./lib/Ship')
 
 function love.load()
   math.randomseed(os.time())
+  game = { time = 0 }
   player = Player:new()
   ship = Ship:new()
 end
 
 function love.update(dt)
+  game.time = game.time + dt
   player:update()
   ship:update()
 end
 
 function love.draw()
-    player:draw()
-    ship:draw()
+  love.graphics.print(game.time, 20, 20) -- prints game time
+  player:draw()
+  ship:draw()
 end
