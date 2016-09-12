cab6533f26300b12a30a8a2d3fc6039edbde0761
Matheus E. Muller <hello@memuller.com>
2016-09-11 23:58:17 -0300

    Spawns ships, if needed.

--- main.lua
+++ main.lua
@@ -4,19 +4,29 @@ local Ship = require('./lib/Ship')
 
 function love.load()
   math.randomseed(os.time())
-  game = { time = 0 }
+  game = { time = 0 , ships = {}}
   player = Player:new()
-  ship = Ship:new()
 end
 
 function love.update(dt)
   game.time = game.time + dt
+  
+  if Ship.shouldSpawn() then
+    table.insert(game.ships, Ship:new())
+  end
+
+  for i,v in ipairs(game.ships) do
+    v:update()
+  end
+
   player:update()
-  ship:update()
 end
 
 function love.draw()
-  love.graphics.print(game.time, 20, 20) -- prints game time
+  love.graphics.print(game.time, 5, 5) -- prints game time
+
+  for i,v in ipairs(game.ships) do
+    v:draw()
+  end
   player:draw()
-  ship:draw()
 end
